501ba429ccd560b3b296afaf1111aa2139180dfd
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-07-31 16:19:42 +1000

    More scripts added. Golem added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform player;
+    public string fileName = "GameData.xml";
+    private GameData data = new GameData();
+
+    public void Start()
+    {
+        Load(Application.dataPath + "/" + fileName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            player = FindObjectOfType<PlayerScript>().transform;
+            data.position = player.position;
+            data.rotation = player.rotation;
+            data.dialogue = new string[]
+            {
+                "Hello",
+                "World!"
+            };
+            Save(Application.dataPath + "/" + fileName);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Load(Application.dataPath + "/" + fileName);
+            player = FindObjectOfType<PlayerScript>().transform;
+            player.position = data.position;
+            player.rotation = data.rotation;
+        }
+    }
+
+    public void Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(GameData));
+        var stream = new FileStream(path, FileMode.Open);
+        data = (GameData)serializer.Deserialize(stream);
+        stream.Close();
+        Debug.Log("Successfully Loaded from " + path);
+    }
+
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof(GameData));
+        var stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, data);
+        stream.Close();
+        Debug.Log("File Successfully created in " + path);
+    }
+}
---
+++ Assets/Scripts/LookScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookScript : MonoBehaviour
+{
+    public float mouseSensitivity = 2.0f;
+    public float minimumY = -90f;
+    public float maximumY = 90f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+    private GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Camera cam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
